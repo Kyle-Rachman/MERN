55158a57d81b7dbe35530af9baf37ba5a2817e80
Kyle-Rachman <kcrachman@gmail.com>
2023-07-02 16:39:28 -0700

    Uploading a bunch of stuff!

---
+++ React/Functional_Components/prop_it_up/src/components/PersonCard.jsx
@@ -0,0 +1,15 @@
+import '../App.css'
+import React from 'react';
+
+const PersonCard = (props) => {
+    const {firstName, lastName, age, hairColor} = props;
+    return (
+        <div className='PersonCard'>
+            <h2>{ lastName }, { firstName }</h2>
+            <p>Age: { age }</p>
+            <p>Hair Color: { hairColor }</p>
+        </div>
+    )
+}
+
+export default PersonCard
---
+++ React/Intro/first_jsx/first_jsx/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Solve a Rubik's Cube without looking up how</li>
+        <li>Become a master climber</li>
+        <li>Write a book</li>
+        <li>Forge a sword</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
