1e90c70f6e94332ade55249c4ad2567294f032b8
Kyle-Rachman <kcrachman@gmail.com>
2023-07-02 19:52:43 -0700

    Changes

--- React/Functional_Components/putting_it_together/src/App.jsx
+++ React/Functional_Components/putting_it_together/src/App.jsx
@@ -5,10 +5,10 @@ function App() {
 
   return (
     <div className='App'>
-      <PersonCard firstName={ 'Jane' } lastName = { 'Doe' } age = { 45 } hairColor = { 'Black' }/>
-      <PersonCard firstName={ 'John' } lastName = { 'Smith' } age = { 88 } hairColor = { 'Brown' }/>
-      <PersonCard firstName={ 'Millard' } lastName = { 'Fillmore' } age = { 50 } hairColor = { 'Brown' }/>
-      <PersonCard firstName={ 'Maria' } lastName = { 'Smith' } age = { 62 } hairColor = { 'Brown' }/>
+      <PersonCard firstName={ 'Jane' } lastName = { 'Doe' } personAge = { 45 } hairColor = { 'Black' }/>
+      <PersonCard firstName={ 'John' } lastName = { 'Smith' } personAge = { 88 } hairColor = { 'Brown' }/>
+      <PersonCard firstName={ 'Millard' } lastName = { 'Fillmore' } personAge = { 50 } hairColor = { 'Brown' }/>
+      <PersonCard firstName={ 'Maria' } lastName = { 'Smith' } personAge = { 62 } hairColor = { 'Brown' }/>
     </div>
   );
 }
--- React/Functional_Components/putting_it_together/src/components/PersonCard.jsx
+++ React/Functional_Components/putting_it_together/src/components/PersonCard.jsx
@@ -1,13 +1,16 @@
 import '../App.css'
-import React from 'react';
+import React, { useState } from 'react';
 
 const PersonCard = (props) => {
-    const {firstName, lastName, age, hairColor} = props;
+    const {firstName, lastName, personAge, hairColor} = props;
+    const [age, setAge] = useState(personAge)
+    const handleClick = () => { setAge(age + 1) }
     return (
         <div className='PersonCard'>
             <h2>{ lastName }, { firstName }</h2>
             <p>Age: { age }</p>
             <p>Hair Color: { hairColor }</p>
+            <button onClick={ handleClick }>Birthday button for { firstName } { lastName }</button>
         </div>
     )
 }
