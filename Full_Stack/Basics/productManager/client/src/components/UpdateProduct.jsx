54b5e624cc5dc86ba143d6854425af9310fc7a0f
Kyle-Rachman <kcrachman@gmail.com>
2023-08-08 11:26:28 -0700

    Small typo

--- Full_Stack/Basics/productManager/client/src/components/UpdateProduct.jsx
+++ Full_Stack/Basics/productManager/client/src/components/UpdateProduct.jsx
@@ -46,15 +46,15 @@ const UpdateProduct = (props) => {
             <h1>Update Product</h1>
             <form onSubmit={handleSubmit}>
                 <div className="form-element">
-                    <label htmlFor="title">Title:</label>
+                    <label htmlFor="title">Title: </label>
                     <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)}/>
                 </div>
                 <div className="form-element">
-                    <label htmlFor="price">Price (in $):</label>
+                    <label htmlFor="price">Price (in $): </label>
                     <input type="number" id="price" step="0.01" value={price} onChange={(e) => setPrice(e.target.value)}/>
                 </div>
                 <div className="form-element">
-                    <label htmlFor="description">Description:</label>
+                    <label htmlFor="description">Description: </label>
                     <input type="text" id="description" value={description} onChange={(e) => setDescription(e.target.value)}/>
                 </div>
                 <button type="submit">Update</button>
